9e964bea1c645708aab3f5ce616ec2d818669433
DayaCravi <cravidayana@gmail.com>
2023-04-10 20:16:33 -0300

    agregando el proyecto

---
+++ e2e/googleSerch.cy.js
@@ -0,0 +1,8 @@
+
+describe('google search cat',()=> {
+  it('wiki gatos',() => {
+    cy.visit('https://www.google.com.uy/')
+    cy.xpath('//input[contains(@class,"gLFyf")]').type('Cat Wiki{enter}')
+    cy.xpath('//h3/ancestor::a[contains(@href,"https://en.wikipedia.org/wiki/Cat")]').click()
+      })
+})
