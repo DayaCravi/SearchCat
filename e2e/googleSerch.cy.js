3495ab0603907941a2a43533a7b8ec2929d83c75
esteban <steban.rg@gmail.com>
2023-04-12 16:10:40 -0400

    test message
    test message

--- e2e/googleSerch.cy.js
+++ e2e/googleSerch.cy.js
@@ -1,8 +1,11 @@
 
 describe('google search cat',()=> {
-  it('wikipedia gatos',() => {
-    cy.visit('https://www.google.com.uy/')
-    cy.xpath('//input[contains(@class,"gLFyf")]').type('Cat Wiki{enter}')
-    cy.xpath('//h3/ancestor::a[contains(@href,"https://en.wikipedia.org/wiki/Cat")]').click()
-      })
+  it('wikipedia gatos', () => {
+    cy.visit('https://www.google.com.uy/');
+    cy.xpath('//input[contains(@class,"gLFyf")]').type('Cat Wiki{enter}');
+    cy.xpath(
+      '//h3/ancestor::a[contains(@href,"https://en.wikipedia.org/wiki/Cat")]'
+    ).click();
+  });
+  // last try
 })
